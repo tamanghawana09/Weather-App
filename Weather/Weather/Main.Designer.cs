64205180ac03ebb8b1e8ca8dad28bab5d7f30dad
ergophile-geek <hawanatamang@gmail.com>
2023-07-01 19:13:00 +0545

    Date and time added

--- Weather/Weather/Main.Designer.cs
+++ Weather/Weather/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Weather
 {
-    partial class main
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -29,11 +29,164 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.gunaElipse2 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gunaElipse3 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.Timer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.Transparent;
+            this.iconButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.ForeColor = System.Drawing.Color.Wheat;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.CircleMinus;
+            this.iconButton1.IconColor = System.Drawing.Color.Red;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 30;
+            this.iconButton1.Location = new System.Drawing.Point(970, 0);
+            this.iconButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(31, 27);
+            this.iconButton1.TabIndex = 1;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // gunaElipse1
+            // 
+            this.gunaElipse1.TargetControl = this;
+            // 
+            // gunaElipse2
+            // 
+            this.gunaElipse2.Radius = 10;
+            this.gunaElipse2.TargetControl = this.comboBox1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 15;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Temperature",
+            "Windy",
+            "Cloudy",
+            "Humidity",
+            "Pressure"});
+            this.comboBox1.Location = new System.Drawing.Point(722, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(223, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(43, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(656, 32);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Start entering city name, US zip code, Canada postal code, UK postcode, ip, metar" +
+    ", etc.";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // gunaElipse3
+            // 
+            this.gunaElipse3.Radius = 15;
+            this.gunaElipse3.TargetControl = this.textBox1;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.iconButton2.FlatAppearance.BorderSize = 0;
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.LocationDot;
+            this.iconButton2.IconColor = System.Drawing.Color.White;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 28;
+            this.iconButton2.Location = new System.Drawing.Point(660, 24);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(28, 31);
+            this.iconButton2.TabIndex = 4;
+            this.iconButton2.UseVisualStyleBackColor = false;
+            this.iconButton2.MouseHover += new System.EventHandler(this.iconButton2_MouseHover);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.BackColor = System.Drawing.Color.Transparent;
+            this.Timer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Timer.Font = new System.Drawing.Font("Yu Gothic UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Timer.Location = new System.Drawing.Point(43, 77);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(234, 128);
+            this.Timer.TabIndex = 5;
+            this.Timer.Text = "7:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(60, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Day";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "main";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 597);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.iconButton2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.iconButton1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private Guna.UI.WinForms.GunaElipse gunaElipse1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private Guna.UI.WinForms.GunaElipse gunaElipse2;
+        private System.Windows.Forms.TextBox textBox1;
+        private Guna.UI.WinForms.GunaElipse gunaElipse3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private System.Windows.Forms.Label Timer;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Weather/Weather/Main.cs
+++ Weather/Weather/Main.cs
@@ -10,11 +10,59 @@ using System.Windows.Forms;
 
 namespace Weather
 {
-    public partial class main : Form
+    public partial class Main : Form
     {
-        public main()
+        public Main()
         {
             InitializeComponent();
+        }
+
+        private void iconButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Start entering city name, US zip code, Canada postal code, UK postcode, ip, metar, etc.")
+            {
+                textBox1.Text = " ";
+            }
+            else if (textBox1.Text == " ")
+            {
+                textBox1.Text = "Start entering city name, US zip code, Canada postl code, UK postcode, ip, metar, etc.";
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Interval = 1000; //1000 milliseconds
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            comboBox1.Size = new System.Drawing.Size(223, 32);
+
+            label1.Text = DateTime.Now.ToString("dddd, MMMM M, yyyy");
+            
+        }
+
+        private void iconButton2_MouseHover(object sender, EventArgs e)
+        {
+            iconButton2.ForeColor = Color.PaleTurquoise;
+            iconButton2.BackColor = Color.PaleTurquoise;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Timer.Text = DateTime.Now.ToString("HH:MM:ss");
         }
     }
 }
